1cac191a89422a6c50d6b19f004bb9bbf7420479
sbanerjee2711 <51205837+suvro2711@users.noreply.github.com>
2026-09-21 12:57:37 +0530

    initial commit

--- models/customers.sql
+++ models/customers.sql
@@ -11,13 +11,7 @@ with customers as (
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
 
---
+++ models/stg_orders.sql
@@ -0,0 +1,9 @@
+{{ config(materialized='ephemeral') }}
+
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+
+from raw.jaffle_shop.orders
